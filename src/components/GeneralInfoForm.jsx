1410c156191bcd0ae4f6197ba70b284b406427d3
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-02-17 18:27:36 -0700

    Formatted GeneralInfoForm.jsx

--- src/components/GeneralInfoForm.jsx
+++ src/components/GeneralInfoForm.jsx
@@ -1,39 +1,51 @@
 import "../styles/GeneralInfoForm.css";
 
 function GeneralInfoForm({ cvData, setCvData }) {
+  function handleChange(e) {
+    setCvData({
+      ...cvData,
+      generalInfo: {
+        ...cvData.generalInfo,
+        [e.target.name]: e.target.value,
+      },
+    });
+  }
 
-    function handleChange(e) {
-        setCvData({
-            ...cvData,
-            generalInfo: {
-                ...cvData.generalInfo,
-                [e.target.name]: e.target.value
-            }
-        });
-    }
-
-    return (
-        <>
-        <h4>General Information</h4>
-        <form className="formContainer">
-            <input className="inputField" name="name" value={cvData.generalInfo.name} onChange={handleChange} placeholder="Full Name" />
-            <input className="inputField" name="address" value={cvData.generalInfo.address} onChange={handleChange} placeholder="Address" />
-            <input className="inputField" name="email" value={cvData.generalInfo.email} onChange={handleChange} placeholder="Email" />
-            <input className="inputField" name="phone" value={cvData.generalInfo.phone} onChange={handleChange} placeholder="Phone" />
-        </form>
-        </>
-    );
+  return (
+    <>
+      <h4>Personal Information</h4>
+      <form className="formContainer">
+        <input
+          className="inputField"
+          name="name"
+          value={cvData.generalInfo.name}
+          onChange={handleChange}
+          placeholder="Full Name"
+        />
+        <input
+          className="inputField"
+          name="address"
+          value={cvData.generalInfo.address}
+          onChange={handleChange}
+          placeholder="Address"
+        />
+        <input
+          className="inputField"
+          name="email"
+          value={cvData.generalInfo.email}
+          onChange={handleChange}
+          placeholder="Email"
+        />
+        <input
+          className="inputField"
+          name="phone"
+          value={cvData.generalInfo.phone}
+          onChange={handleChange}
+          placeholder="Phone"
+        />
+      </form>
+    </>
+  );
 }
 
 export default GeneralInfoForm;
-
-
-
-
-
-
-
-
-
-
-
