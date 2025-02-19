698b25f6b5f3791f1fa5d0fb4dd04670f118955a
janpetallo <101074771+janpetallo@users.noreply.github.com>
2025-02-18 21:02:23 -0700

    Implemented condiitional rendering on CVPreview
    Ensures Section Headers and |'s do not appear
    until needed

--- src/components/CVPreview.jsx
+++ src/components/CVPreview.jsx
@@ -6,54 +6,62 @@ function CVPreview({ cvData }) {
       <div className="cvHeader">
         <div className="name">{cvData.generalInfo.name}</div>
         <div>
-          {cvData.generalInfo.address} | {cvData.generalInfo.email} |{" "}
-          {cvData.generalInfo.phone}
+          {cvData.generalInfo.address && <span>{cvData.generalInfo.address}</span>}
+          {cvData.generalInfo.address && (cvData.generalInfo.email || cvData.generalInfo.phone) && <span> | </span>}
+          {cvData.generalInfo.email && <span>{cvData.generalInfo.email}</span>}
+          {cvData.generalInfo.email && cvData.generalInfo.phone && <span> | </span>}
+          {cvData.generalInfo.phone && <span>{cvData.generalInfo.phone}</span>}
         </div>
       </div>
       <div className="cvBody">
-        <div className="sectionContainer">
-          <div className="sectionHeader">Work Experience</div>
-          <hr className="horizontalLine" />
-          {cvData.experience.map((exp) => (
-            <div className="experience" key={exp.id}>
-              <div className="experienceHeader">
-                <div className="position-company">
-                  <div className="position">{exp.position}</div>
-                  <div className="company">
-                    {exp.company}, {exp.location}
+        {cvData.experience.length > 0 && (
+          <div className="sectionContainer">
+            <div className="sectionHeader">Work Experience</div>
+            <hr className="horizontalLine" />
+            {cvData.experience.map((exp) => (
+              <div className="experience" key={exp.id}>
+                <div className="experienceHeader">
+                  <div className="position-company">
+                    <div className="position">{exp.position}</div>
+                    <div className="company">
+                      {exp.company}, {exp.location}
+                    </div>
+                  </div>
+                  <div>
+                    {exp.startDate} - {exp.endDate}
                   </div>
                 </div>
-                <div>
-                  {exp.startDate} - {exp.endDate}
-                </div>
+                <ul>
+                  {exp.descriptions.map((desc) => (
+                    <li key={desc.id}>{desc.text}</li>
+                  ))}
+                </ul>
               </div>
-              <ul>
-                {exp.descriptions.map((desc) => (
-                  <li key={desc.id}>{desc.text}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-        <div className="sectionContainer">
-          <div className="sectionHeader">Education</div>
-          <hr className="horizontalLine" />
-          {cvData.education.map((edu) => (
-            <div className="education" key={edu.id}>
-              <div className="educationeHeader">
-                <div className="program-school">
-                  <div className="program">{edu.program}</div>
-                  <div className="school">
-                    {edu.school}, {edu.location}
+            ))}
+          </div>
+        )}
+
+        {cvData.education.length > 0 && (
+          <div className="sectionContainer">
+            <div className="sectionHeader">Education</div>
+            <hr className="horizontalLine" />
+            {cvData.education.map((edu) => (
+              <div className="education" key={edu.id}>
+                <div className="educationeHeader">
+                  <div className="program-school">
+                    <div className="program">{edu.program}</div>
+                    <div className="school">
+                      {edu.school}, {edu.location}
+                    </div>
+                  </div>
+                  <div>
+                    {edu.startDate} - {edu.endDate}
                   </div>
                 </div>
-                <div>
-                  {edu.startDate} - {edu.endDate}
-                </div>
               </div>
-            </div>
-          ))}
-        </div>
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
